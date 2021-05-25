10e6cface4214da5e8e1b16ff07f199350e44a30
Bryanggm2617 <74894661+Bryanggm2617@users.noreply.github.com>
2021-05-25 01:35:02 -0500

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -3,6 +3,6 @@ from flask import request
 sample = Flask(__name__)
 @sample.route("/")
 def main():
-    return "You are calling me from - " + request.remote_addr + "\n"
+    return "You are calling me from " + request.remote_addr + "\n"
 if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=8080)
